3e8205bd67a0ece028bcdedd8a8aae4b0809ddb8
Chaima201021 <chernichaima95@gmail.com>
2026-05-25 15:25:51 +0100

    fix server.js + clean node_modules + production setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 let db;
 
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
